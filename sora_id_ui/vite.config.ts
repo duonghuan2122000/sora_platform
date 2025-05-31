74d8a24a44c384ceda1a8ad6f7d74309adb80f42
Jocelyn <duong.huan2122000@gmail.com>
2025-05-31 11:05:08 +0700

    #feat(sora_id_ui): Cập nhật base path

--- sora_id_ui/vite.config.ts
+++ sora_id_ui/vite.config.ts
@@ -1,25 +1,32 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
-import path from 'path';
+import { defineConfig, loadEnv } from "vite";
+import solid from "vite-plugin-solid";
+import path from "path";
 
-export default defineConfig({
-  plugins: [solid()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
-  // Optional: Silence Sass deprecation warnings. See note below.
-  css: {
-    preprocessorOptions: {
-      scss: {
-        silenceDeprecations: [
-          'import',
-          'mixed-decls',
-          'color-functions',
-          'global-builtin',
-        ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    base: env.VITE_BASE_URL,
+    plugins: [solid()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
       },
     },
-  },
-})
+    // Optional: Silence Sass deprecation warnings. See note below.
+    css: {
+      preprocessorOptions: {
+        scss: {
+          silenceDeprecations: [
+            "import",
+            "mixed-decls",
+            "color-functions",
+            "global-builtin",
+          ],
+        },
+      },
+    },
+    server: {
+      port: 8081,
+    },
+  };
+});
